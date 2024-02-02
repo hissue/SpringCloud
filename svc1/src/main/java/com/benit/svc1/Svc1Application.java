247033fd20110321f1ff8f08fd6200affc6cac19
hissue <wldmx@naver.com>
2024-02-02 21:15:56 +0900

    Feat : svc1 프로젝트 초기 생성

---
+++ svc1/src/main/java/com/benit/svc1/Svc1Application.java
@@ -0,0 +1,14 @@
+package com.benit.svc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svc1Application {
+
+	public static void main(String[] args) {
+		//First Service 실행
+		SpringApplication.run(Svc1Application.class, args);
+	}
+
+}
